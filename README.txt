9c3b0601ed31783f83f806f20df473c22fd392e3
SoobumNa <sb051380us@gmail.com>
2024-06-11 20:21:29 +0900

    wip

--- README.txt
+++ README.txt
@@ -1,2 +1,5 @@
 # 0-90-180-270-prediction (incomplete)
 This project provides semi-rotation-invariant object detection algorithm based on YOLOv8.
+It is pretty simple. Just rotate image for 0, 90, 180, 270 degree respectively,
+predict by those images,
+and merge those four results into one.
--- predict.py
+++ predict.py
@@ -1,6 +1,41 @@
-import ultralytics
+from cv2.typing import MatLike
+from ultralytics import YOLO, ASSETS
+from ultralytics.engine.results import Results
+import os
+import cv2
 
-yolo = ultralytics.YOLO()
-results = yolo()
-for result in results:
+yolo = YOLO()
+
+def classic_predict(yolo:YOLO, im:MatLike) -> Results:
+    results = yolo(im)
+    assert len(results) == 1
+    result = results[0]
+    return result
+
+def rotating_predict(yolo:YOLO, im:MatLike) -> Results:
+    # Predict for four angles respectively.
+    result0 = classic_predict(yolo, im)
+    im = cv2.rotate(im, cv2.ROTATE_90_CLOCKWISE)
+
+    result1 = classic_predict(yolo, im)
+    im = cv2.rotate(im, cv2.ROTATE_90_CLOCKWISE)
+
+    result2 = classic_predict(yolo, im)
+    im = cv2.rotate(im, cv2.ROTATE_90_CLOCKWISE)
+
+    result3 = classic_predict(yolo, im)
+    
+    
+
+    return result0
+
+assets = os.listdir(ASSETS)
+
+for asset in assets:
+    abspath = os.path.join(ASSETS, asset)
+    im = cv2.imread(abspath)
+
+    result = rotating_predict(yolo, im)
+    print("result=", result)
+    print("result.boxes=", result.boxes)
     result.show()
